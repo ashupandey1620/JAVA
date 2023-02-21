9914fd57d49a4e10cf4041ebccee3c682b60b747
Ashutosh Pandey <ashupandey1620@gmail.com>
2023-02-21 10:58:34 +0530

    modified:   Backtracking/Subsetproblem.java

--- Backtracking/Subsetproblem.java
+++ Backtracking/Subsetproblem.java
@@ -1,8 +1,36 @@
 package Backtracking;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
-
 public class Subsetproblem {
-    Scanner sc = new Scanner(System.in);
-    
+    public static void main(String [] args)
+    {
+      Scanner sc =new Scanner(System.in);
+      int arr[]={1,2,3,4};
+      List<List<Integer>> ans = new ArrayList<>();
+      ans = Subsetproblem.subsets(arr);
+      System.out.println(ans);
+       sc.close();
+    }
+    public static List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> powerSet = new ArrayList<>();
+         List<Integer> subSet = new ArrayList<>();
+         solve(nums,0,subSet,powerSet);
+         return powerSet;
+    }
+    public static void solve(int[] nums,int i,List<Integer> subSet,List<List<Integer>> powerSet)
+    {
+        if(i==nums.length)
+        {
+        powerSet.add(new ArrayList<>(subSet));
+        return;
+        }
+        else{
+            subSet.add(nums[i]);
+            solve(nums,i+1,subSet,powerSet);
+            subSet.remove(subSet.size()-1);
+            solve(nums,i+1,subSet,powerSet);    
+        }
+    }
 }
